54bbf94c1a885f392b0168b0114b6ad1c8373760
Bach <bachp2@uw.edu>
2017-03-23 12:21:17 -0700

    it werks

--- src/Conjugation.java
+++ src/Conjugation.java
@@ -1,4 +1,4 @@
-import org.w3c.dom.Document;
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -27,57 +27,6 @@ public class Conjugation {
     private List <List <String>> rads_vs = new ArrayList <>();
     //list of verbs, radicals and template names
 
-    enum Mode {
-        infinitive("infinitive-present"),
-        indicative("present", "imperfect", "future", "simple-past"),
-        conditional("present"),
-        subjunctive("present", "imperfect"),
-        imperative("imperative-present"),
-        participle("present-participle", "past-participle");
-        private String[] tenses;
-
-        Mode(String... tenses) {
-            this.tenses = tenses;
-        }
-
-        public int getTenseIndex(String tense) {
-            if (tense.equals("present")) {
-                switch (this) {
-                    case infinitive:
-                        tense = tense + "-present";
-                        break;
-                    case imperative:
-                        tense = "imperative-" + tense;
-                        break;
-                    case participle:
-                        tense = tense + "-participle";
-                    default:
-                        break;
-                }
-            } else if (tense.equals("past")) {
-                switch (this) {
-                    case indicative:
-                        tense = "simple-" + tense;
-                        break;
-                    case participle:
-                        tense = tense + "-participle";
-                        break;
-                    default:
-                        break;
-                }
-            }
-            int i = 0;
-            for (String s : tenses) {
-                if (tense.equals(s))
-                    return i++;
-            }
-            throw new NoSuchElementException("tense not found");
-        }
-
-        public int length() {
-            return tenses.length;
-        }
-    }
     /**
      * empty constructor, when initiated will be use for the entire operation
      */
@@ -166,54 +115,77 @@ public class Conjugation {
 
     /**
      * @param templateName
-     * @param modeIndex
-     * @param tenseIndex
+     * @param mode
+     * @param tense
      * @return
      */
     public String[][] listOfPrefix(String templateName,
-                                   int modeIndex,
-                                   int tenseIndex) {
-        String[][] p = new String[6][];
-        for (int i = 0; i < nConj.getLength(); i++) {
-            Node temp = nConj.item(i);
-            Element tmp = (Element) temp;
-            String other = temp.getAttributes().getNamedItem("name")
-                    .getNodeValue();
-            if (templateName.equals(other)) {
-                Element md = (Element) tmp.getChildNodes().item(modeIndex);
-                Element ten = (Element) md.getChildNodes().item(tenseIndex);
-                NodeList listP = ten.getElementsByTagName("p");
-                for (int j = 0; j < listP.getLength(); j++) {
-                    boolean isType = (listP.item(j).getNodeType() == Node.ELEMENT_NODE);
-                    if (isType) {
-                        Element person = (Element) listP.item(j);
-                        if (person.hasChildNodes()) {
-                            NodeList listI = person
-                                    .getElementsByTagName("i");
+                                   String mode,
+                                   String tense) {
+        Node temp;
+        Element tmp, md, ten, person;
+        NodeList listP, listI;
+        String other;
+        try {
+            String[][] p = new String[6][];
+            for (int i = 0; i < nConj.getLength(); i++) {
+                temp = nConj.item(i);
+                tmp = (Element) temp;
+                other = temp.getAttributes().getNamedItem("name")
+                        .getNodeValue();
+                if (templateName.equals(other)) {
+                    md = (Element) tmp.getElementsByTagName(mode).item(0);
+                    ten = (Element) md.getElementsByTagName(tense).item(0);
+                    listP = (NodeList) ten.getElementsByTagName("p");
+                    listI = null;
+                    for (int j = 0; j < listP.getLength(); j++) {
+                        boolean isType = (listP.item(j).getNodeType() == Node.ELEMENT_NODE);
+                        if (isType) {
+                            person = (Element) listP.item(j);
+                            if (person.hasChildNodes()) {
+                                listI = person
+                                        .getElementsByTagName("i");
+                            }
                             p[j] = new String[listI.getLength()];
                             for (int k = 0; k < listI.getLength(); k++) {
                                 p[j][k] = listI.item(k).getTextContent();
                             }
                         }
                     }
+                    return p;
                 }
-                break;
             }
+            throw new NoSuchElementException();
+        } catch (DOMException e) {
+            e.printStackTrace();
+            return null;
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            temp = null;
+            tmp = null;
+            md = null;
+            ten = null;
+            person = null;
+            listP = null;
+            listI = null;
+            other = null;
         }
-        return p;
     }
 
     /**
      *
      */
     public String toString() {
+        if(_listConjugation == null) return "";
         String out = "";
         String[] pronouns = {"je", "tu", "il", "nous", "vous", "ils"};
         for (int i = 0; i < _listConjugation.length; i++) {
             if (_listConjugation[i] != null) {
                 out = pronouns[i] + " ";
                 for (int j = 0; j < _listConjugation[i].length; j++) {
-                    out+= _listConjugation[i][j];
+                    out += _listConjugation[i][j];
                     if (j < _listConjugation[i].length - 1
                             && _listConjugation[i].length > 1)
                         out += "/";
@@ -227,7 +199,6 @@ public class Conjugation {
     //todo : strip accent for input.
 
     /**
-     *
      * @return
      */
     public List <List <String>> setListRad_and_Verbs() {
@@ -252,36 +223,76 @@ public class Conjugation {
 
     //get NodeList of verbs-fr and conjugation-fr
 
-    /**
-     *
-     * @param verb
-     */
-//    public void displayAll(String verb) {
-//        String[] modes = {"infinitive", "indicative", "conditional",
-//                "subjunctive", "imperative", "participle"};
-//        int count = 0;
-//        for (String m : modes) {
-//            String[] tenses = mode.get(m);
-//            for (String t : tenses) {
-//                System.out.printf("%d. %s %s", count++, m, t);
-//                System.out.println();
-//                display();
-//            }
-//        }
-//    }
+    enum Mode {
+        infinitive("infinitive-present"),
+        indicative("present", "imperfect", "future", "simple-past"),
+        conditional("present"),
+        subjunctive("present", "imperfect"),
+        imperative("imperative-present"),
+        participle("present-participle", "past-participle");
+        private String[] tenses;
+
+        Mode(String... tenses) {
+            this.tenses = tenses;
+        }
+
+        /**
+         * get index of a given mode
+         *
+         * @param mode
+         * @return
+         */
+        public static int indexOf(Object mode) {
+            if (mode instanceof Mode) {
+                int i = 0;
+                for (Mode m : Mode.values()) {
+                    if (m == mode) return i;
+                    i++;
+                }
+            }
+            return -1;
+        }
+
+        public static Mode getMode(String mode) {
+            try {
+                for (Mode m : Mode.values()) {
+                    if (mode.equals(m.toString()))
+                        return m;
+                }
+                throw new NoSuchElementException();
+            } catch (NoSuchElementException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        public String getTense(int index) {
+            if(index < this.length() && index >= 0){
+                return tenses[index];
+            }
+            else throw new NoSuchElementException();
+        }
+
+        public int length() {
+            return tenses.length;
+        }
+    }
+
     public class Deconjugation {
         private Trie verb_trie;
-        public Deconjugation(){
+
+        public Deconjugation() {
             verb_trie = new Trie();
-            for(List<String> rad_v : rads_vs){
+            for (List <String> rad_v : rads_vs) {
                 verb_trie.insert(rad_v.get(1));
             }
         }
-        public String[] match(String verb){
+
+        public String[] match(String verb) {
             String[] verb_templateName = new String[2];
             String temp = verb_trie.search(verb);
-            for(List<String> rad_v : rads_vs){
-                if(rad_v.contains(temp)){
+            for (List <String> rad_v : rads_vs) {
+                if (rad_v.contains(temp)) {
                     verb_templateName[0] = rad_v.get(0);
                     verb_templateName[1] = rad_v.get(2);
                     return verb_templateName;
@@ -296,7 +307,25 @@ public class Conjugation {
      * @param args
      */
     public static void main(String[] args) {
-
+        Conjugation conj = new Conjugation();
+        String tn = conj.search("abjurer");
+        String[][] p = conj.listOfPrefix(tn, "indicative", "future");
+        for (int i = 0; i < p.length; i++) {
+            if (p[i] != null) {
+                for (int j = 0; j < p[i].length; j++) {
+                    System.out.println(p[i][j]);
+                }
+            }
+        }
+        String rad = trim(tn, "abjurer");
+        System.out.println(rad);
+        String[][] f = conj.append(rad, p);
+        for (int i = 0; i < f.length; i++) {
+            if (f[i] != null) {
+                for (int j = 0; j < f[i].length; j++) {
+                    System.out.println(p[i][j]);
+                }
+            }
+        }
     }
-
 }
